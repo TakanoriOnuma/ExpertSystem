e675097e45cdf1449e543aad2c138b57192df50e
TakanoriOnuma <s1000607@sendai-nct.jp>
2014-06-20 11:47:45 +0900

    ルールごとに文章を登録する
    R1ならR1に関する情報をとりあえずそのまま入れるようにした。
    
    Signed-off-by: TakanoriOnuma <s1000607@sendai-nct.jp>

--- searching.py
+++ searching.py
@@ -2,10 +2,27 @@
 
 import io
 
+rule = {}
 f = io.open('animal.rule', 'r', encoding = 'utf_8_sig')
 
-for line in f:
-    words = line.split('\t')
-    for word in words:
-        print word,
+line = f.readline()
+while line:
+    if line.startswith(u'R', 0, 1):
+        break
+    line = f.readline()
 
+# ルールを分類
+while line:
+    R  = line[0:line.find('\t')]
+    li = [line]
+    line = f.readline()
+    while line.startswith(u'R', 0, 1) == False and line:
+        li.append(line)
+        line = f.readline()
+    rule[R] = li
+rule[R] = li
+
+for r in rule:
+    print r,
+    for word in rule[r]:
+        print word,
