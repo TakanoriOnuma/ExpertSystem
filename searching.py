632ef4073ca176a4b26f57fdb8904c6a789e7f42
TakanoriOnuma <s1000607@sendai-nct.jp>
2014-06-20 11:56:36 +0900

    1行にしたルールをline_ruresにして格納
    1つのルールを1行で表現したため、後はここから分割していく。
    
    Signed-off-by: TakanoriOnuma <s1000607@sendai-nct.jp>

--- searching.py
+++ searching.py
@@ -2,7 +2,7 @@
 
 import io
 
-rule = {}
+line_rules = []
 f = io.open('animal.rule', 'r', encoding = 'utf_8_sig')
 
 line = f.readline()
@@ -11,16 +11,14 @@ while line:
         break
     line = f.readline()
 
-# ルールを分類
+# ルールを分割
 while line:
-    R  = line[0:line.find('\t')]
     string = line[0:-1]
     line = f.readline()
     while line.startswith(u'R', 0, 1) == False and line:
         string += line[0:-1]
         line = f.readline()
-    rule[R] = string
-rule[R] = string
+    line_rules.append(string)
 
-for r in rule:
-    print r, rule[r]
+for r in line_rules:
+    print r
