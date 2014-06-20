099a6fb3a96686c6f60857edef4c0ac25c2fd4db
TakanoriOnuma <s1000607@sendai-nct.jp>
2014-06-20 11:07:44 +0900

    各単語をリスト化して表示
    取りあえず\tでstlipして分割した。
    まだ改行や空白データも残っているため、その辺を直していく。
    
    Signed-off-by: TakanoriOnuma <s1000607@sendai-nct.jp>

--- searching.py
+++ searching.py
@@ -2,5 +2,10 @@
 
 import io
 
-for line in io.open('animal.rule', 'r', encoding = 'utf_8_sig'):
-    print line,
+f = io.open('animal.rule', 'r', encoding = 'utf_8_sig')
+
+for line in f:
+    words = line.split('\t')
+    for word in words:
+        print word,
+
